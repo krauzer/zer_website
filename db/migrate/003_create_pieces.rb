2bcc9ff9113d9ad34cc60b6b177ababbb46d6f46
Dylan Krause <dykrause@gmail.com>
2014-09-03 19:42:05 -0400

    Set Piece associations and database attributes

--- db/migrate/003_create_pieces.rb
+++ db/migrate/003_create_pieces.rb
@@ -1,6 +1,6 @@
 class CreatePieces < ActiveRecord::Migration
   def change 
-  	create table :pieces do |t|
+  	create_table :pieces do |t|
   		t.string :body 
   		t.string :addition 
   		t.references :topic
