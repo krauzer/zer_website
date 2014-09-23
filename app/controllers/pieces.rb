f40fc6ed3ce42066c79b69a3e04267677c684673
Dylan Krause <dykrause@gmail.com>
2014-09-23 13:38:36 -0400

    Add show route for pieces in pieces controller based on id

--- app/controllers/pieces.rb
+++ app/controllers/pieces.rb
@@ -1,3 +1,14 @@
 Website::App.controllers :pieces do 
 
+  get :show, map: "/pieces/:id" do 
+    markdown = create_parser 
+    @piece = Piece.find(params[:id]
+    unless @piece.nil? 
+      @title = @piece.title
+      @text = markdown.render(@piece.body).html_safe
+      @addition = markdown.render(@piece.addition).html_safe unless @piece.addition.nil?
+    end
+    erb :"pieces/show"
+  end
+
 end
