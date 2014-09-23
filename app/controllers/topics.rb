1c0e7db5d49cf6e0bcd110a9a49eedf374cf6abc
Dylan Krause <dykrause@gmail.com>
2014-09-22 21:42:32 -0400

    Add controller routes for index and individual show for topics -use markdown render to output text into html -redirect to 404 page if not found -use frame for index page

--- app/controllers/topics.rb
+++ app/controllers/topics.rb
@@ -1,5 +1,20 @@
 Website::App.controllers :log do 
 
+	get :index do 
+		markdown = create_parser 
+		@piece = Piece.find_by_title("Log")
+		@topics = Frame.find_by_name("main").sub_topics
+		unless @piece.nil? 
+			@text = markdown.render(@piece.body).html_safe
+		end
+		erb :"topics/index"
+	end 
 
+	get :show, map: "/log/:topic" do
+		@topic = Topic.find_by_name(params[:topic])
+		redirect "errors/404" if @topic.nil? 
+		@topics = @topic.sub_topics 
+		erb :"topics/show"
+	end 
 
 end
