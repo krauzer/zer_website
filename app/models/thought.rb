5e4c6ed6c060e904b8a86002f394b7d1633f2b21
Dylan Krause <dykrause@gmail.com>
2014-09-24 20:49:00 -0400

    Add class method to thougth that returns entries from the current day

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,6 +1,11 @@
 class Thought < ActiveRecord::Base
   before_create :record_day
-	
+
+  def self.from_today
+    today = Time.now.to_date
+    self.where(day: today)
+  end
+
   private 
   def record_day
     self.day = Time.now.to_date
