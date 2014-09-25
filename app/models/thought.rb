9180f486e7544964cb42941e35593c824034366d
Dylan Krause <dykrause@gmail.com>
2014-09-24 23:32:22 -0400

    Add method that collects all the thoughts from last week -collects and returns AR relations in an object -current day set to 1 as marker

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -6,6 +6,24 @@ class Thought < ActiveRecord::Base
     self.where(day: today)
   end
 
+  def self.last_week
+    week = {}
+    Date::DAYNAMES.each do |day|
+      if Date::DAYNAMES[Time.now.to_date.wday]
+        week[day] = 1
+      else
+        week[day] = nil
+      end
+    end
+    seven_days = []
+    6.times{|x| seven_days << Time.now.to_date.prev_day(x+1)}
+    seven_days.each do |date|
+    	day_name = Date::DAYNAMES[date.wday]
+    	week[day_name] = self.where(day: date)
+    end
+    week
+  end
+
   def day_name 
     Date::DAYNAMES[day.wday]
   end
