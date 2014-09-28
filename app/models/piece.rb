6c73b7047aeb75d6f864a48157479a0aedae53fd
Dylan Krause <dykrause@gmail.com>
2014-09-27 23:08:02 -0400

    Add autofill for day attribute of pieces model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,8 +1,14 @@
 class Piece < ActiveRecord::Base
   has_many :topic_pieces 
   has_many :topics, through: :topic_pieces
+  before_create :record_day
 
   def body_sample
   	body.slice(/\A.*\./)
+  end
+
+  private 
+  def record_day 
+    self.day = Time.now.to_date
   end
 end
