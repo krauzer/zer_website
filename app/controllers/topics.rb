9df2ebf1242a7c326b2efe46fc3b876dabbda5b5
Dylan Krause <dykrause@gmail.com>
2014-09-30 19:02:51 -0400

    Add nil check to main frame in order to call sub topics

--- app/controllers/topics.rb
+++ app/controllers/topics.rb
@@ -1,23 +1,24 @@
-Website::App.controllers :log do 
+Website::App.controllers :log do
 
-	get :index do 
-		markdown = create_parser 
+	get :index do
+		markdown = create_parser
 		@piece = Piece.find_by_title("Log")
-		@topics = Frame.find_by_name("main").sub_topics
-		unless @piece.nil? 
+    @main = Frame.find_by_name("main")
+		@topics = @main.sub_topics unless @main.nil?
+		unless @piece.nil?
 			@text = markdown.render(@piece.body).html_safe
 		end
 		erb :"topics/index"
-	end 
+	end
 
   get :show, map: "/log/:topic" do
     @topic = Topic.find_by_name(params[:topic])
     redirect "errors/404" if @topic.nil?
-    @piece =  @topic.pieces.first
-    unless @piece.nil? 
+    @piece =  @topic.pieces.last #change to all
+    unless @piece.nil?
       @text = @piece.body_sample
     end
-    @topics = @topic.sub_topics 
+    @topics = @topic.sub_topics
     erb :"topics/show"
-  end 
+  end
 end
