4ab6753dadb8efea8a71f19fe7dcff9215a85469
Dylan Krause <dykrause@gmail.com>
2014-09-30 18:57:16 -0400

    Add date formating method to piece models

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,5 +1,5 @@
 class Piece < ActiveRecord::Base
-  has_many :topic_pieces 
+  has_many :topic_pieces
   has_many :topics, through: :topic_pieces
   before_create :record_day
 
@@ -7,8 +7,16 @@ class Piece < ActiveRecord::Base
   	body.slice(/\A.*\./)
   end
 
-  private 
-  def record_day 
+  def proper_date
+    date = day.to_s
+    year = date[2,2]
+    dy = date[-2,2]
+    month = date[5,2]
+    "#{month}/#{dy}"
+  end
+
+  private
+  def record_day
     self.day = Time.now.to_date
   end
 end
