ac29cc61d0da2fc5d8556152f123cc93c68f4346
Dylan Krause <dykrause@gmail.com>
2014-11-09 23:40:47 -0500

    DRY up stories controller with private methods -store story info in an object and render from that in the view show page

--- app/controllers/stories.rb
+++ app/controllers/stories.rb
@@ -1,20 +1,35 @@
-Website::App.controllers :stories do 
+Website::App.controllers :stories do
 	layout :stories
 
-  get :index do 
-    @stories = Story.all
+  get :index do
+    load_stories
     erb :"stories/index"
-  end 
-  
+  end
+
   layout :pieces
   get :show, map: "/stories/:id" do
-    markdown = create_parser 
-    @story = Story.find(params[:id])
-    unless @story.nil? 
-      @text = markdown.render(@story.body).html_safe
-      @title = @story.title
-    end
+    load_story
+    parse_story
     erb :"stories/show"
-  end 
+  end
+
+
+private
+  define_method :load_stories do
+    @stories ||= Story.all
+  end
+
+  define_method :load_story do
+    @story ||= Story.find(params[:id])
+  end
+
+  define_method :parse_story do
+    markdown ||= create_parser
+    unless @story.nil?
+      @content = Hash.new
+      @content[:title] = @story.title
+      @content[:text] = markdown.render(@story.body).html_safe
+    end
+  end
 
 end
