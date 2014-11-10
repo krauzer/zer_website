9e575b44c5be1f162827137fa576fce074c2b707
Dylan Krause <dykrause@gmail.com>
2014-11-09 23:48:59 -0500

    DRY up thoughts controller by seperating out functionality into private methods

--- app/controllers/thoughts.rb
+++ app/controllers/thoughts.rb
@@ -1,16 +1,35 @@
-Website::App.controllers :thoughts do 
+Website::App.controllers :thoughts do
 
   get :index do
-    @current_day = Thought.from_today
-    @full_week = Thought.last_week
+    load_thought_from_today
+    load_thought_from_week
     erb :"thoughts/index"
   end
- 
+
   layout :pieces
   get :show , map: "/thoughts/:day" do
-  	markdown = create_parser
-    @thought = Thought.find_day(params[:day]).first
-   	@text = markdown.render(@thought.body).html_safe unless @thought.nil?
-    erb :"thoughts/show" 
+    load_thought
+   	parse_thought
+    erb :"thoughts/show"
   end
+
+private
+
+  define_method :load_thought_from_today do
+    @current_day = Thought.from_today
+  end
+
+  define_method :load_thought_from_week do
+    @full_week = Thought.last_week
+  end
+
+  define_method :load_thought do
+    @thought ||= Thought.find_day(params[:day]).first
+  end
+
+  define_method :parse_thought do
+    markdown ||= create_parser
+    @text = markdown.render(@thought.body).html_safe unless @thought.nil?
+  end
+
 end
