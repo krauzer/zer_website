a1c0e0c6097c75462f9499654f19809e1ef914d2
Dylan Krause <dykrause@gmail.com>
2014-11-09 22:53:57 -0500

    DRY up pieces controller by seperating functionality out into private methods -store info in one object and alter pieces show page to reflect this

--- app/controllers/pieces.rb
+++ app/controllers/pieces.rb
@@ -1,15 +1,26 @@
-Website::App.controllers :pieces do 
+Website::App.controllers :pieces do
   layout :pieces
 
-  get :show, map: "/pieces/:id" do 
-    markdown = create_parser 
-    @piece = Piece.find(params[:id])
-    unless @piece.nil? 
-      @title = @piece.title
-      @text = markdown.render(@piece.body).html_safe
-      @addition = markdown.render(@piece.addition).html_safe unless @piece.addition.nil?
-    end
+  get :show, map: "/pieces/:id" do
+    load_piece
+    parse_piece
     erb :"pieces/show"
+  end
+
+private
+
+  define_method :load_piece do
+    @piece ||= Piece.find(params[:id])
+  end
+
+  define_method :parse_piece do
+    markdown ||= create_parser
+    unless @piece.nil?
+      @content = Hash.new
+      @content[:title] = @piece.title
+      @content[:text] = markdown.render(@piece.body).html_safe
+      @content[:addition] = markdown.render(@piece.addition).html_safe unless @piece.addition.nil?
+    end
   end
 
 end
